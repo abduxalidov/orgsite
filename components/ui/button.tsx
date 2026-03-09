40d8d3450c659feaacbeff48f9d72886c21c6d15
abduxalidov <cryptofast717@gmail.com>
2026-03-09 05:31:02 +0500

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,16 +1,20 @@
 import * as React from "react"
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode
+}
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className = "", ...props }, ref) => {
+  ({ className = "", children, ...props }, ref) => {
     return (
       <button
         ref={ref}
-        className={`inline-flex items-center justify-center rounded-lg px-4 py-2 bg-primary text-white hover:opacity-90 ${className}`}
+        className={`inline-flex items-center justify-center rounded-lg px-4 py-2 bg-primary text-white hover:opacity-90 transition ${className}`}
         {...props}
-      />
+      >
+        {children}
+      </button>
     )
   }
 )
